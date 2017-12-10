a57f7d84c8cc9a7c9aa624ce8348cc6174f5fc41
deb0ch <thomas.de.beauchene@gmail.com>
2017-12-10 16:55:16 +0100

    allow checking, unechecking and removing tasks

--- imports/ui/Task.js
+++ imports/ui/Task.js
@@ -1,10 +1,32 @@
 
 import React, { Component } from 'react';
 
+import { Tasks } from '../api/tasks.js'
+
+// Task component - represents a single item
 export default class Task extends Component {
     render() {
         return (
-            <li>{this.props.task.text}</li>
+            <li className={this.props.task.checked ? "checked" : ""}>
+                <button className="delete"
+                        onClick={this.remove.bind(this)}>
+                &times;
+                </button>
+                <input type="checkbox"
+                       readOnly
+                       checked={Boolean(this.props.task.checked)}
+                       onChange={this.toggleChecked.bind(this)}/>
+                <span className="text"> {this.props.task.text} </span>
+            </li>
         );
+    }
+
+    toggleChecked() {
+        Tasks.update(this.props.task._id,
+                     {$set:{checked:!this.props.task.checked}});
+    }
+
+    remove() {
+        Tasks.remove(this.props.task._id);
     }
 }
