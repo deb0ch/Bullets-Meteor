1eaee5a20cf2d86bdb2f3f3da6e1a6de95369aeb
deb0ch <thomas.de.beauchene@gmail.com>
2017-12-10 15:45:58 +0100

    start using react with code from meteor tutorial step 2

--- client/main.js
+++ client/main.js
@@ -1,22 +1,11 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
 
-import './main.html';
+import React from 'react';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+import { Meteor } from 'meteor/meteor';
+import { render } from 'react-dom';
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
+import App from '../imports/ui/App.js';
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Meteor.startup(() => {
+    render(<App/>, document.getElementById('render-target'));
 });
