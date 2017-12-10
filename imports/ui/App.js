ab2ceb3a7278de26f177c1a27e4022b52d4c4e0b
deb0ch <thomas.de.beauchene@gmail.com>
2017-12-10 22:08:03 +0100

    add visibility selector to show/hide checked tasks

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -5,12 +5,29 @@ import { withTracker } from 'meteor/react-meteor-data';
 import Task from './Task.js';
 import { Tasks } from '../api/tasks.js';
 
+
+const VisibilitySelector = props => (
+    <label className="hide-completed">
+        <input type="checkbox"
+               readOnly
+               checked={props.hideChecked}
+               onChange={props.handleToggleVisibility}
+        />
+        Hide checked
+    </label>
+);
+
+
 class App extends Component {
     render() {
         return (
             <div className='container'>
                 <header>
                     <h1>Todo List</h1>
+                    <VisibilitySelector checked={this.state.hideChecked}
+                                        handleToggleVisibility={
+                                            this.handleToggleVisibility.bind(this)
+                                        }/>
                     <form className="new-task"
                           onSubmit={this.onFormSubmit.bind(this)}>
                         <input type="text"
@@ -20,9 +37,14 @@ class App extends Component {
                     </form>
                 </header>
                 <ul> {
-                    this.props.tasks.map((task) => (
-                        <Task key={task._id} task={task}/>
-                    ))
+                    (() => {
+                        let filteredList = this.props.tasks.filter(item => (
+                            !this.state.hideChecked || !item.checked
+                        ));
+                        return filteredList.map((task) => (
+                            <Task key={task._id} task={task}/>
+                        ));
+                    })()
                 } </ul>
             </div>
         );
@@ -32,6 +54,7 @@ class App extends Component {
         super(props);
         this.state = {
             inputText: "",
+            hideChecked: false,
         };
     }
 
@@ -47,6 +70,10 @@ class App extends Component {
 
     onFormChange(event) {
         this.setState({inputText: event.target.value});
+    }
+
+    handleToggleVisibility() {
+        this.setState({hideChecked: !this.state.hideChecked})
     }
 }
 
