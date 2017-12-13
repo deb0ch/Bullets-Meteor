32a53cccbe864e17fd411d3c6bd7c5d8ab7a5bbd
deb0ch <thomas.de.beauchene@gmail.com>
2017-12-13 10:56:49 +0100

    add first unit test

---
+++ imports/api/tasks.tests.js
@@ -0,0 +1,38 @@
+/* eslint-env mocha */
+
+import { Meteor } from 'meteor/meteor';
+import { Random } from 'meteor/random';
+import { assert } from 'chai';
+
+import { Tasks } from './tasks.js';
+
+if (Meteor.isServer) {
+    describe('Tasks', () => {
+        describe('methods', () => {
+            const userId = Random.id();
+            let taskId;
+
+            beforeEach(() => {
+                Tasks.remove({});
+                taskId = Tasks.insert({
+                    text: 'test task',
+                    createdAt: new Date(),
+                    owner: userId,
+                    username: 'tmeasday',
+                });
+            });
+
+            it('can delete owned task', () => {
+                // Find the internal implementation of the task method so we can
+                // test it in isolation
+                const deleteTask = Meteor.server.method_handlers['tasks.remove'];
+                // Set up a fake method invocation that looks like what the
+                // method expects
+                const invocation = { userId };
+                // `this` set to the fake invocation
+                deleteTask.apply(invocation, [taskId]);
+                assert.equal(Tasks.find().count(), 0);
+            });
+        });
+    });
+}
