815fc81fce1b4f3250ca6bcc4cf9d838fb0c5c2d
deb0ch <thomas.de.beauchene@gmail.com>
2017-12-28 18:30:29 +0100

    AccountsUIWrapper: rename container => accounts-ui

--- imports/ui/AccountsUIWrapper.js
+++ imports/ui/AccountsUIWrapper.js
@@ -7,16 +7,15 @@ export default class AccountsUIWrapper extends Component {
     componentDidMount() {
         // Use Meteor Blaze to render login buttons
         this.view = Blaze.render(Template.loginButtons,
-                                 ReactDOM.findDOMNode(this.refs.container));
+                                 ReactDOM.findDOMNode(this.refs.accounts-ui));
     }
 
     componentWillUnmount() {
-        // Clean up Blaze view
         Blaze.remove(this.view);
     }
 
     render() {
-        // Just render a placeholder container that will be filled in
-        return <span ref="container" />;
+        // render a placeholder container that will be filled in
+        return <span ref="accounts-ui" />;
     }
 }
